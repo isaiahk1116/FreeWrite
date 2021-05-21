8c051351c7bf0bc257472a50c1f5672c1d4b4c8c
isaiahk1116 <i.a.khan308@gmail.com>
2021-05-20 20:35:23 -0400

    timer changes

--- client/src/components/Write/Write.js
+++ client/src/components/Write/Write.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Form from '../Form/Form';
 import Timer from '../Timer/Timer';
 
 const Write = () => {
+    const [timesUp, setTimesUp] = useState(false);
+
     return (
         <div className="write-container">
-            <Timer />
-            <Form />
+            <Timer timesUp={timesUp} setTimesUp={setTimesUp} />
+            <Form timesUp={timesUp} />
         </div>
     );
 };
